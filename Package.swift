ba8b174ee492df74827a7589a09178e6c34c737d
ScoreNow-EU <139147379+ScoreNow-EU@users.noreply.github.com>
2024-06-18 13:20:04 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,36 +1,31 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
     name: "MySQL",
     products: [
-        .library(name: "MySQL", targets: ["MySQL"])
+        .library(name: "MySQL", targets: ["MySQL"]),
     ],
     dependencies: [
         .package(url: "https://github.com/novi/cmysql.git", from: "2.0.0"),
     ],
     targets: [
         .target(
-            name: "SQLFormatter"
+            name: "SQLFormatter",
+            dependencies: []
         ),
         .target(
             name: "MySQL",
-            dependencies: [
-                "SQLFormatter",
-            ]
+            dependencies: ["SQLFormatter", "cmysql"]
         ),
         .testTarget(
             name: "MySQLTests",
-            dependencies: [
-                "MySQL"
-            ]
+            dependencies: ["MySQL"]
         ),
         .testTarget(
             name: "SQLFormatterTests",
-            dependencies: [
-                "MySQL"
-            ]
-        )
+            dependencies: ["SQLFormatter"]
+        ),
     ],
-    swiftLanguageVersions: [4]
+    swiftLanguageVersions: [.v5]
 )
